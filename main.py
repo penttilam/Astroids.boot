b9166986c460d1ef344233fb2e9dc347556f9919
penttilam <michael.penttila@gmail.com>
2026-02-16 13:58:57 -0800

    added character ship to screen

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT /2
+    player = Player(x, y)
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -18,6 +22,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
