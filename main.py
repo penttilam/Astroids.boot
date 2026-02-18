109b678b187e93d4b54dc664ce4364d712e030a9
penttilam <michael.penttila@gmail.com>
2026-02-18 13:30:53 -0800

    added rotation to ship

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
